c39001f2525a948c9039f62e6778ff6f44688e43
Andy Fragen <andy@thefragens.com>
2020-02-22 14:51:41 -0800

    update for next wp-dependency-installer

--- README.md
+++ README.md
@@ -5,12 +5,14 @@
  * Description: Allows you to easily add a group of plugins to a WordPress installation.
  * Author: Andy Fragen
  * License: MIT
- * Requires WP: 4.7
+ * Requires WP: 5.1
  * Requires PHP: 5.6
  * GitHub Plugin URI: afragen/group-plugin-installer
 
 ## Description
 
 Allows you to easily add a group of plugins to a WordPress installation by using [wp-dependency-installer framework](https://github.com/afragen/wp-dependency-installer).
+
+Also see [WP Dependency Installer Examples](https://github.com/afragen/wp-dependency-installer-examples).
 
 You can change the plugins that are installed by editing the `wp-dependencies.json` file. Instructions are in the framework link above.
--- group-plugin-installer.php
+++ group-plugin-installer.php
@@ -13,23 +13,34 @@
  * Plugin URI: https://github.com/afragen/group-plugin-installer
  * Description: Allows you to easily add a group of plugins to a WordPress installation.
  * Author: Andy Fragen
- * Version: 0.2.4
+ * Version: 0.3.0
  * License: MIT
  * Domain Path: /languages
  * Text Domain: group-plugin-installer
  * Network: true
- * Requires at least: 4.7
+ * Requires at least: 5.1
  * Requires PHP: 5.6
  * GitHub Plugin URI: afragen/group-plugin-installer
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
-WP_Dependency_Installer::instance()->run( __DIR__ );
+WP_Dependency_Installer::instance( __DIR__ )->run();
+
 add_filter(
 	'wp_dependency_timeout',
 	function( $timeout, $source ) {
 		$timeout = basename( __DIR__ ) !== $source ? $timeout : 14;
 		return $timeout;
+	},
+	10,
+	2
+);
+
+add_filter(
+	'wp_dependency_dismiss_label',
+	function( $label, $source ) {
+		$label = basename( __DIR__ ) !== $source ? $label : __( 'Group Plugin Installer', 'group-plugin-installer' );
+		return $label;
 	},
 	10,
 	2
